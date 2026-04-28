6652fc7029fa29677ad14c7a8275a3e6fa1cd3d7
Wagner R. da Silva <wagaum@gmail.com>
2026-04-27 21:25:26 -0300

    semaforo

--- lacos.py
+++ lacos.py
@@ -1,1 +1,10 @@
-print("Olá GitHub")
+sinal =in
+
+if sinal == "vermelho":
+    print("PARE!")
+elif sinal == "amarelo":
+    print("ATENÇÃO!")
+elif sinal == "verde":
+    print("Pode passar")
+else:
+    print("Que sinal é caboclo?")
